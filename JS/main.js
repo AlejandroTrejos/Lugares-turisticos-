18746a23d916ef5dfcb2439da3effd177f5cd457
isaias <isaiasjre2511@gmail.com>
2020-12-07 22:31:17 -0500

    pagina web

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+const panama = document.querySelector('#panama')
+const chiriqui = document.querySelector('#chiriqui')
+const bocas = document.querySelector('#bocas')
+const ngobe = document.querySelector('#ngobe')
+const ls = document.querySelector('#ls')
+const veraguas = document.querySelector('#veraguas')
+const colon = document.querySelector('#colon')
+const west = document.querySelector('#west')
+
+// REDIRECCIONADORES
+panama.addEventListener('click',function(){
+    window.open('Panama.html' ,'_self')
+})
+chiriqui.addEventListener('click',function(){
+    window.open('Chiriqui.html' ,'_self')
+})
+veraguas.addEventListener('click',function(){
+    window.open('Veraguas.html' ,'_self')
+})
+bocas.addEventListener('click',function(){
+    window.open('BocasDelToro.html' ,'_self')
+})
+ngobe.addEventListener('click',function(){
+    window.open('Ngobe-bugle.html' ,'_self')
+})
+colon.addEventListener('click',function(){
+    window.open('Colon.html' ,'_self')
+})
+west.addEventListener('click',function(){
+    window.open('West.html' ,'_self')
+})
+ls.addEventListener('click',function(){
+    window.open('los-santos.html' ,'_self')
+})
